f354a8faf6e42b3ef5663e5636d3e2cba7e7e85e
Daniel Leussa <leussadaniel04@gmail.com>
2024-02-06 15:19:28 +0100

    Ajout du composant ExploreMenu

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import HomeBanner from "./components/HomeBanner.jsx";
+import ExploreMenu from "./components/ExploreMenu.jsx";
 
 export default function Home()
 {
     return (
         <>
             <HomeBanner />
+            <ExploreMenu />
         </>
     )
 }
