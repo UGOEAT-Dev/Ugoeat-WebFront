e9c9c89faa0e585cb7aba246ad66082840d7ac39
Daniel Leussa <leussadaniel04@gmail.com>
2024-02-06 18:08:00 +0100

    Le Header reste desormais fixe lors des scrolls

--- src/pages/root/Root.jsx
+++ src/pages/root/Root.jsx
@@ -1,14 +1,16 @@
 import {Outlet} from "react-router-dom";
 import Header from "./components/Header.jsx";
 import './root.scss'
+import {useEffect} from "react";
 
 /**
  * Represent The base of the web Application
  */
 export default function Root()
 {
+
     return <>
         <Header/>
-        <div><Outlet/></div>
+        <main><Outlet/></main>
     </>
 }
--- src/pages/root/components/Header.jsx
+++ src/pages/root/components/Header.jsx
@@ -6,9 +6,9 @@ export default function Header({onNavBarBtnClicked = null})
 {
 
     return (
-        <header className="lg:flex lg:bg-transparent bg-gray-50 justify-evenly p-3 items-center">
+        <header className="lg:flex lg:fixed lg:top-0 z-10 w-full bg-transparent lg:bg-black lg:bg-opacity-70 bg-gray-50 justify-evenly p-3 items-center">
             <div className="flex cursor-pointer items-center space-x-2" onClick={onNavBarBtnClicked}>
-                <AiOutlineMenu size={24} className="block lg:d-none"/>
+                <AiOutlineMenu size={24} className="block lg:hidden"/>
                 <img src={logoWhite} alt="logo-white" width="64"/>
             </div>
             <Navbar className="lg:w-5/6 lg:mt-3" />
