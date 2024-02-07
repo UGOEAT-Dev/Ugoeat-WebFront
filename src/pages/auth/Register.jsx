6c22607b4dd16f9075ce5350f83a539c288b600e
Daniel Leussa <leussadaniel04@gmail.com>
2024-02-07 09:20:59 +0100

    Ajout de la page d'inscription

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,5 +1,31 @@
 
+import InputWithLabel from "../../components/InputWithLabel.jsx";
+import {Link} from "react-router-dom";
+import bgRegister from "../../assets/images/bg-register.png"
+
 export default function Register()
 {
-    return <h2>Register</h2>
+    return (
+        <div className="bg-center bg-cover sm:min-h-screen flex sm:items-center justify-center py-5 lg:pt-24 lg:pb-10 px-2" style={{
+            backgroundImage: `url(${bgRegister})`,
+        }}>
+            <form className="rounded-md shadow-xl bg-white w-full md:w-[600px] px-5 py-10 space-y-5" action="#">
+                <h2 className="text-center text-2xl font-bold">Inscrivez-vous</h2>
+                <InputWithLabel id="name" label="Noms et Prenoms" name="name" placeholder="Ex: John Doe" />
+                <InputWithLabel id="email" label="Email" type="email" name="email" placeholder="name@company" />
+                <InputWithLabel id="password" label="Mot de passe" type="password" name="password" placeholder="*******" />
+                <InputWithLabel id="password_confirmation" label="Confirmer le mot de passe" name="password_confirmation" placeholder="*******" />
+                <div className="hidden justify-between text-sm">
+                    <div>
+                        <input type="checkbox" id="keepOnline" name="keepOnline" />
+                        &thinsp;&thinsp;
+                        <label htmlFor="keepOnline">Rester Connecter</label>
+                    </div>
+                    <Link to="#" className="text-blue-600">Mot de passe oublie?</Link>
+                </div>
+                <button type="submit" className="font-bold text-white bg-green p-3 rounded-md w-full hover:bg-gray-950">S&apos;inscrire</button>
+                <p className="text-sm font-bold">Vous avez deja un compte? <Link to="/login" className="text-blue-600">Connectez vous</Link></p>
+            </form>
+        </div>
+    )
 }
