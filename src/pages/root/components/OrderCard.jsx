4a3ef8579bea16e8d6cd5805a8f699d9f0c1b74e
Daniel Leussa <leussadaniel04@gmail.com>
2024-02-09 18:34:39 +0100

    Creation du composant OrderProductCardList qui liste l'integralite des produits dans le composant OrderCard.jsx

--- src/pages/root/components/OrderCard.jsx
+++ src/pages/root/components/OrderCard.jsx
@@ -1,27 +1,25 @@
-import {useMemo} from "react";
-import OrderProductCard from "./OrderProductCard.jsx";
+import {useEffect, useMemo, useState} from "react";
+import OrderProductCardList from "./OrderProductCardList.jsx";
 
-function OrderCard({products = [], onConfirmBtnClicked = null, onCancelBtnClicked = null, onItemCancelBtnClicked = null, ...props})
+function OrderCard({products = [], onConfirmBtnClicked, onCancelBtnClicked, onItemCancelBtnClicked, ...props})
 {
     const total = useMemo(() => {
-        let sum = 0;
-        products.forEach((p) => sum += p.price * p.quantity)
+        let sum = 0
+        products.forEach(p => sum += p.price)
         return sum
     }, [products])
 
     return (
         <div className={"flex flex-col gap-2 rounded-md shadow-md p-5 " + props.className}>
             <div>
-                {products.map((p) => {
-                    return ( <OrderProductCard onClick={onItemCancelBtnClicked} key={p.id} product={p}/>)
-                })}
+                {products && <OrderProductCardList products={products} onClick={onItemCancelBtnClicked} /> }
             </div>
             <div className="flex justify-between p-2 font-bold">
                 <span>TOTAL</span>
                 <span>{total || 0} XAF</span>
             </div>
             <button onClick={onConfirmBtnClicked} className="bg-green text-white p-2 font-bold rounded-md hover:bg-green-600">Confirmer</button>
-            <button onClick={onCancelBtnClicked} className="bg-gray-100 text-red-600 p-2 hover:bg-gray-200 rounded-md">Vider le panier</button>
+            <button onClick={() => onCancelBtnClicked()} className="bg-gray-100 text-red-600 p-2 hover:bg-gray-200 rounded-md">Vider le panier</button>
         </div>
     )
 }
