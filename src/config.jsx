164490e7cc657baa370c10903d10e217db0fe369
Daniel Leussa <leussadaniel04@gmail.com>
2024-02-09 18:24:18 +0100

    Implementation d'axios et du fichier de configuration de base config.jsx

---
+++ src/config.jsx
@@ -0,0 +1,15 @@
+
+const config = {
+
+    'LARAVEL_BACKEND_URL': 'http://localhost:8000',
+    'axios': {
+        'headers': {
+            'Accept': 'application/json',
+            /* 'User-Agent': 'XMLHttpRequest', */
+        },
+
+    }
+
+}
+
+export { config }
