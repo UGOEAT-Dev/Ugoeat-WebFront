3c0a7a7d8af2ee8a22047101d4fd1c0b9aec971b
Daniel Leussa <leussadaniel04@gmail.com>
2024-02-09 18:29:56 +0100

    Implementation de l'AppContext qui fourni les objets a access global a l'application

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@
 import {RouterProvider} from "react-router-dom";
 import router from "./router.jsx";
 import './scss/app.scss'
+import {useLocalStorage} from "./hooks/useLocalStorage.jsx";
+import AppContext from "./AppContext.jsx";
 
 export default function App()
 {
-    window.order = { products: [] }
+    const [orders, setOrders] = useLocalStorage('orders',{ products: [] })
 
-    return <RouterProvider router={router}/>
+    return (
+        <AppContext.Provider value={{orders, setOrders}}>
+            <RouterProvider router={router}/>
+        </AppContext.Provider>
+    )
+
 }
