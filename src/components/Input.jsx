5b46cdf7a287737802ecc4e2705530f3aed44adc
Daniel Leussa <leussadaniel04@gmail.com>
2024-02-13 18:16:18 +0100

    Ajout de l'attribut ref a mes composants Inputs

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,10 @@
 
-function Input({id, name, placeholder = "", type = "text", icon = null, className = "", onChange = null})
+function Input({ref, id, name, placeholder = "", type = "text", icon = null, className = "", onChange = null})
 {
     return (
         <div className={"rounded flex items-center bg-gray-100 w-[250px] p-1 " + className}>
             {icon}
-            <input id={id} name={name} type={type} placeholder={placeholder} className="bg-transparent w-full mx-1 focus:outline-none" onChange={onChange} />
+            <input ref={ref} id={id} name={name} type={type} placeholder={placeholder} className="bg-transparent w-full mx-1 focus:outline-none" onChange={onChange} />
         </div>
     )
 }
--- src/components/InputWithLabel.jsx
+++ src/components/InputWithLabel.jsx
@@ -1,12 +1,12 @@
 import Input from "./Input.jsx";
 import {BiCheck, BiError} from "react-icons/bi";
 
-function InputWithLabel({label, name, type = "text", success = null, error = null, placeholder = "", id = null, className = "", onChange = null})
+function InputWithLabel({label, name, type = "text", success, error, placeholder, id , className, onChange, ref})
 {
     return (
         <div className={"flex flex-col items-start space-y-2 " + className}>
             <label className={`font-bold`} htmlFor={id}>{label}</label>
-            <Input onChange={onChange} id={id} name={name} type={type} placeholder={placeholder} className={`w-full py-2 ${error? 'input-error' : success ? 'input-success' : ''}`} />
+            <Input ref={ref} onChange={onChange} id={id} name={name} type={type} placeholder={placeholder} className={`w-full py-2 ${error? 'input-error' : success ? 'input-success' : ''}`} />
             <span className={`${error ? 'error' : success ? 'success' : 'hidden'}  flex items-center gap-2 font-bold`}>
                 {error ? <BiError/> : <BiCheck /> }
                 {error || success}
