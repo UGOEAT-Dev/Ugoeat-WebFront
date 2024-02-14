8dd67f3b453d3b9558cea443dacee284cb28a736
Daniel Leussa <leussadaniel04@gmail.com>
2024-02-14 15:33:57 +0100

    Ajout de nouvelles fonctions dans le Helper, pour mettre de verifier l'objet user

--- src/lib/helpers.jsx
+++ src/lib/helpers.jsx
@@ -8,12 +8,68 @@ function calculateTotal(products = [])
 
 function isUserLoggedIn(user, token = '')
 {
-    return (user !== null)
-            && (Object.keys(user).length !== 0)
-            && token
+    return isUserObject(user) && token
+}
+
+function isUserObject(user)
+{
+    const obj = Object(user)
+    return obj.hasOwnProperty('id')
+            && obj.hasOwnProperty('name')
+            && obj.hasOwnProperty('address')
+            && obj.hasOwnProperty('image_url')
+            && obj.hasOwnProperty('role')
+            && obj.hasOwnProperty('tel')
+            && obj.hasOwnProperty('email_verified_at')
+}
+
+function hasEmail(user) { return user.email && isUserObject(user) }
+
+function hasId(user) { return user.id && isUserObject(user) }
+
+function hasName(user) { return user.name && isUserObject(user) }
+
+function hasAddress(user) { return user.address && isUserObject(user) }
+
+function hasRole(user) { return user.role && isUserObject(user) }
+
+function hasImage(user) { return user.image_url && isUserObject(user) }
+
+function hasTel(user) { return user.tel && isUserObject(user) }
+
+
+function checkUserProfileComplete(user)
+{
+    const callbacks = [
+        hasId, hasEmail, hasTel, hasName,
+        hasAddress, hasRole, hasImage
+    ]
+    const returns = Array(callbacks.length)
+    callbacks.forEach((callback) => returns.push(callback(user)))
+
+    return {
+        state: returns.filter((v) => v === true ).length,
+        count: callbacks.length,
+        states: returns,
+        conditions: callbacks
+    }
+}
+
+function calculcateProfileCompletedPercentage(state, count)
+{
+    return state / count
 }
 
 export {
     calculateTotal,
     isUserLoggedIn,
+    checkUserProfileComplete,
+    calculcateProfileCompletedPercentage,
+    hasImage,
+    hasRole,
+    hasAddress,
+    hasName,
+    hasTel,
+    hasEmail,
+    hasId
 }
