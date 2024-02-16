ecd03385f7e41b083f15b0ccb7c84c68ff57eab0
Daniel Leussa <leussadaniel04@gmail.com>
2024-02-16 08:34:16 +0100

    mise a jour des composants Inputs

--- src/components/InputWithLabel.jsx
+++ src/components/InputWithLabel.jsx
@@ -1,12 +1,12 @@
 import Input from "./Input.jsx";
 import {BiCheck, BiError} from "react-icons/bi";
 
-function InputWithLabel({label, name, type = "text", success, error, placeholder, id , className, onChange, ref})
+function InputWithLabel({label, name, value, type = "text", success, error, placeholder, id , className, onChange, ref, ...props})
 {
     return (
         <div className={"flex flex-col items-start space-y-2 " + className}>
             <label className={`font-bold`} htmlFor={id}>{label}</label>
-            <Input ref={ref} onChange={onChange} id={id} name={name} type={type} placeholder={placeholder} className={`w-full py-2 ${error? 'input-error' : success ? 'input-success' : ''}`} />
+            <Input value={value} ref={ref} onChange={onChange} id={id} name={name} type={type} placeholder={placeholder} className={`w-full py-2 ${error? 'input-error' : success ? 'input-success' : ''}`} {...props} />
             <span className={`${error ? 'error' : success ? 'success' : 'hidden'}  flex items-center gap-2 font-bold`}>
                 {error ? <BiError/> : <BiCheck /> }
                 {error || success}
