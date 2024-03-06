7efee204099c8b67397ea4fe6c5e73b4a6497d8c
Daniel Leussa <leussadaniel04@gmail.com>
2024-03-06 11:10:42 +0100

    {null}

--- src/components/RoundedImage.jsx
+++ src/components/RoundedImage.jsx
@@ -2,7 +2,13 @@
 function RoundedImage({size, ...props})
 {
     return (
-        <div className={`overflow-hidden flex items-center w-[${size}px] h-[${size}px] rounded-full border align-middle`}>
+        <div
+            className={`overflow-hidden flex items-center rounded-full border align-middle`}
+            style={{
+                width: `${size}px`,
+                height: `${size}px`
+            }}
+        >
             <img {...props} className="h-full" />
         </div>
     )
